8b1ca5694f28c633a278896ca2b23e95a5a2bf12
fel-04 <feelipe.jgr@gmail.com>
2026-07-23 02:30:24 -0600

    change: school periods uuid

--- app/Models/SchoolPeriod.php
+++ app/Models/SchoolPeriod.php
@@ -2,13 +2,30 @@
 
 namespace App\Models;
 
+use App\Traits\UsesUuidForRoutes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class SchoolPeriod extends Model
 {
+
+    use HasUuids;
+    use UsesUuidForRoutes;
+
     protected $fillable = [
+        'uuid',
         'name',
         'start_date',
         'end_date',
     ];
+
+    /**
+     * Define cuáles columnas deben recibir un UUID automático.
+     *
+     * @return array
+     */
+    public function uniqueIds(): array
+    {
+        return ['uuid'];
+    }
 }
--- database/migrations/2026_06_16_193418_create_school_periods_table.php
+++ database/migrations/2026_06_16_193418_create_school_periods_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('school_periods', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
             $table->string('name');
             $table->date('start_date');
             $table->date('end_date');
